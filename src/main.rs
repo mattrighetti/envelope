736847c7d882886b9f033f5a40d6639d89f75543
Mattia Righetti <matt95.righetti@gmail.com>
2023-09-21 21:03:12 +0200

    feat: more cmd information

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,25 @@ mod command;
 use clap::Parser;
 use command::EnvelopeCmd;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const SHA: &str = env!("GIT_HASH");
+
+static HELP_TEMPLATE: &str = "\
+{before-help}{name} {version}
+{author}
+{about}
+
+{usage-heading}
+  {usage}
+
+{all-args}{after-help}";
+
 #[derive(Parser)]
+#[command(
+    author = "Mattia Righetti <matt95.righetti@gmail.com>",
+    version = VERSION,
+    help_template(HELP_TEMPLATE),
+)]
 struct Envelope {
     #[command(subcommand)]
     envelope: EnvelopeCmd,
