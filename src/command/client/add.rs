2ec1ab54e1634f01672846d181512a888222c19d
Mattia Righetti <matt95.righetti@gmail.com>
2023-10-06 13:38:33 +0200

    feat: read env variable value from stdin
    + closes #4

--- src/command/client/add.rs
+++ src/command/client/add.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use sqlx::SqlitePool;
+use std::io::{BufRead, Error, ErrorKind, Write};
 
 use crate::ops;
 
@@ -11,17 +12,37 @@ pub struct Cmd {
     /// Name of the environment variable
     key: String,
 
+    /// Read environment variable value from stdin
+    #[arg(short, long)]
+    stdin: bool,
+
     /// Value of the environment variable. Default to empty string if not provided.
     value: Option<String>,
 }
 
 impl Cmd {
-    pub async fn run(&self, db: &SqlitePool) -> std::io::Result<()> {
-        let value = match &self.value {
-            None => "",
-            Some(s) => s,
-        };
+    pub async fn run(&mut self, db: &SqlitePool) -> std::io::Result<()> {
+        if self.stdin && self.value.is_some() {
+            return Err(Error::new(
+                ErrorKind::Other,
+                "can't specify a value if you're reading from stdin",
+            ));
+        }
 
-        ops::add_var(db, &self.env, &self.key, value).await
+        let mut value = String::new();
+        match self.stdin {
+            true => {
+                writeln!(std::io::stdout(), "Enter value for env {}: ", self.env)?;
+                let stdin = std::io::stdin();
+                stdin.lock().read_line(&mut value)?;
+            }
+            false => {
+                if self.value.is_some() {
+                    value = self.value.take().unwrap();
+                }
+            }
+        }
+
+        ops::add_var(db, &self.env, &self.key, &value).await
     }
 }
