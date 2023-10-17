ddc41feb982577b1f0834aefdcce300607f3e22f
Mattia Righetti <matt95.righetti@gmail.com>
2023-10-17 12:49:57 +0200

    refactor: new import function with reader arg

--- src/ops/add.rs
+++ src/ops/add.rs
@@ -2,8 +2,6 @@ use sqlx::SqlitePool;
 use std::io::{self, BufRead, Write};
 use std::io::{Error, ErrorKind};
 
-use super::read_lines;
-
 /// Adds a single key-value element to the database
 ///
 /// If the value of v is None, an empty string is inserted
@@ -23,36 +21,8 @@ pub async fn add_var(db: &SqlitePool, env: &str, k: &str, v: &str) -> io::Result
     Ok(())
 }
 
-/// Imports every key-value from a file at the given path
-pub async fn import_from_file(pool: &SqlitePool, env: &str, path: &str) -> io::Result<()> {
-    let buf = read_lines(path)?;
-    for line in buf {
-        if line.is_err() {
-            continue;
-        }
-
-        if line.as_ref().unwrap().starts_with('#') {
-            writeln!(io::stdout(), "skipping {}", line.unwrap())?;
-            continue;
-        }
-
-        if let Some((k, v)) = line.unwrap().split_once('=') {
-            sqlx::query("INSERT INTO environments(env,key,value) VALUES (?, upper(?), ?);")
-                .bind(env)
-                .bind(k)
-                .bind(v)
-                .execute(pool)
-                .await
-                .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
-        }
-    }
-
-    Ok(())
-}
-
-pub async fn import_from_stdin(pool: &SqlitePool, env: &str) -> io::Result<()> {
-    let buf = io::BufReader::new(io::stdin());
-    for line in buf.lines() {
+pub async fn import<R: BufRead>(reader: R, pool: &SqlitePool, env: &str) -> io::Result<()> {
+    for line in reader.lines() {
         if line.is_err() {
             continue;
         }
--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -11,17 +11,3 @@ pub use duplicate::*;
 pub use export::*;
 pub use list::*;
 pub use sync::*;
-
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
