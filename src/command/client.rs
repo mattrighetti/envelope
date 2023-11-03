5f60487c141f5b5278e0db49f6f0d0f9c9befa51
Mattia Righetti <matt95.righetti@gmail.com>
2023-11-03 01:02:45 +0100

    refactor: alphabetically order sub-commands

--- src/command/client.rs
+++ src/command/client.rs
@@ -15,25 +15,25 @@ mod sync;
 #[derive(Subcommand)]
 #[command(infer_subcommands = true)]
 pub enum EnvelopeCmd {
-    /// Initialize envelope
-    Init,
+    Add(add::Cmd),
 
     /// Check which environment is currently exported
     Check,
 
-    Add(add::Cmd),
-
-    List(list::Cmd),
-
-    Import(import::Cmd),
-
     Delete(delete::Cmd),
 
-    Export(export::Cmd),
+    Drop(drop::Cmd),
 
     Duplicate(duplicate::Cmd),
 
-    Drop(drop::Cmd),
+    Export(export::Cmd),
+
+    /// Initialize envelope
+    Init,
+
+    Import(import::Cmd),
+
+    List(list::Cmd),
 
     Sync(sync::Cmd),
 }
@@ -45,15 +45,15 @@ impl EnvelopeCmd {
             .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
 
         match self {
-            Self::Delete(delete) => delete.run(&db).await?,
-            Self::List(list) => list.run(&db).await?,
             Self::Add(add) => add.run(&db).await?,
-            Self::Import(import) => import.run(&db).await?,
-            Self::Export(export) => export.run(&db).await?,
-            Self::Duplicate(duplicate) => duplicate.run(&db).await?,
-            Self::Drop(drop) => drop.run(&db).await?,
-            Self::Sync(sync) => sync.run(&db).await?,
             Self::Check => ops::check(&mut std::io::stdout(), &db).await?,
+            Self::Delete(delete) => delete.run(&db).await?,
+            Self::Drop(drop) => drop.run(&db).await?,
+            Self::Duplicate(duplicate) => duplicate.run(&db).await?,
+            Self::Export(export) => export.run(&db).await?,
+            Self::Import(import) => import.run(&db).await?,
+            Self::List(list) => list.run(&db).await?,
+            Self::Sync(sync) => sync.run(&db).await?,
             _ => {}
         }
 
