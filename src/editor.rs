533a86e1258f8e7cd318f1c52b08950632dbe875
Mattia Righetti <matt95.righetti@gmail.com>
2023-11-05 11:50:08 +0100

    refactor: misc refactoring

--- src/editor.rs
+++ src/editor.rs
@@ -2,20 +2,49 @@ use std::{
     env::temp_dir,
     fs::{File, OpenOptions},
     io::{BufReader, Result, Write},
-    path::PathBuf,
 };
 
-use crate::other_err;
 use crate::subproc::ChildProcess;
+use crate::{other_err, other_str_err};
 
-fn prepare_file(path: &PathBuf, data: &[u8]) -> Result<()> {
-    let mut file = OpenOptions::new()
+fn envelope_editor() -> String {
+    let editor = "vi";
+
+    if let Some(e) = std::env::var_os("ENVELOPE_EDITOR") {
+        if let Some(e) = e.to_str() {
+            return e.to_string();
+        }
+    }
+
+    if let Some(e) = std::env::var_os("GIT_EDITOR") {
+        if let Some(e) = e.to_str() {
+            return e.to_string();
+        }
+    }
+
+    editor.to_string()
+}
+
+fn writer() -> Result<File> {
+    let pb = temp_dir().join("ENVELOPE_EDITMSG");
+
+    OpenOptions::new()
         .write(true)
         .read(true)
         .create(true)
         .truncate(true)
-        .open(path.clone())?;
+        .open(pb)
+}
 
+fn reader() -> Result<BufReader<File>> {
+    let pb = temp_dir().join("ENVELOPE_EDITMSG");
+    let file = OpenOptions::new().read(true).open(pb)?;
+
+    Ok(BufReader::new(file))
+}
+
+fn prepare_file(data: &[u8]) -> Result<()> {
+    let mut file = writer()?;
     file.write_all(data)?;
     file.write(b"\n\n")?;
     file.write(b"# Comment variables to remove them")?;
@@ -24,13 +53,18 @@ fn prepare_file(path: &PathBuf, data: &[u8]) -> Result<()> {
 }
 
 pub fn spawn_with(data: &[u8]) -> Result<BufReader<File>> {
-    let fp = temp_dir().join("ENVELOPE_EDITMSG");
-    prepare_file(&fp, data)?;
+    prepare_file(data)?;
 
-    let cmd = ChildProcess::new("nvim", &[&fp.to_str().unwrap()], &[]);
-    cmd.run_shell_command()
-        .map_err(|e| other_err!("error running child process", e))?;
+    {
+        let editor = envelope_editor();
+        if let Some(pb) = temp_dir().join("ENVELOPE_EDITMSG").to_str() {
+            let cmd = ChildProcess::new(&editor, &[pb], &[]);
+            cmd.run_shell_command()
+                .map_err(|e| other_err!("error running child process", e))?;
+        } else {
+            return Err(other_str_err!("cannot run editor"));
+        }
+    }
 
-    let file = OpenOptions::new().read(true).open(fp.clone())?;
-    Ok(BufReader::new(file))
+    reader()
 }
