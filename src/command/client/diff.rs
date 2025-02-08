6cf788db7350bf7a4b3620cddc258002b8a745d0
Mattia Righetti <matt95.righetti@gmail.com>
2025-02-08 14:00:56 +0000

    chore: add diff docs

--- src/command/client/diff.rs
+++ src/command/client/diff.rs
@@ -5,10 +5,20 @@ use clap::Parser;
 use crate::db::EnvelopeDb;
 use crate::ops;
 
-/// Diff two environments
+/// Diff two existing environments
+///
+/// This command will list all the variables that differ between source and target environments, order matters.
+///
+/// - Variables present in env1 but not in env2 will be shown in green.
+///
+/// - Variables not present in env1 but present in env2 will be shown in red.
+///
+/// - Variables both present in env1 and env2 but with a different value will be shown in gray, first value being the value in env1, second value is the value found in env2.
 #[derive(Parser)]
 pub struct Cmd {
+    /// Source environment
     env1: String,
+    /// Target environment
     env2: String,
 }
 
