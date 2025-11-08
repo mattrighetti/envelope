34edbc34fdf97a4b36222d77e7c785e3ad8e89f6
Mattia Righetti <matt95.righetti@gmail.com>
2025-11-08 11:51:56 +0000

    chore: update deps
    update dependencies with `cargo update`

--- src/ops/add.rs
+++ src/ops/add.rs
@@ -48,7 +48,7 @@ mod test {
     use std::io::BufReader;
 
     use super::*;
-    use crate::db::{test_db, EnvironmentRow};
+    use crate::db::{EnvironmentRow, test_db};
 
     pub fn stdin_input(s: &str) -> BufReader<&[u8]> {
         BufReader::new(s.as_bytes())
